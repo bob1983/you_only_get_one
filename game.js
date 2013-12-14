27228dd9495ef13879ffebd2108716a721a3ec43
bob_1983 <bob.1983.g@gmail.com>
2013-12-14 21:39:28 +0900

    change game window size

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 enchant();
 
 window.onload = function() {
-    var game = new Game(500, 500);
+    var game = new Game(640, 640);
     game.preload('./background.jpg');
     game.preload('./enchant.js-builds-0.8.0/images/monster/bigmonster1.gif');
     game.preload('./enchant.js-builds-0.8.0/images/icon0.png');
@@ -10,20 +10,19 @@ window.onload = function() {
 
     game.onload = function() {
 
-
         var background = new Sprite(game.rootScene.width, game.rootScene.height);
         background.image = game.assets['./background.jpg'];
         game.rootScene.addChild(background);
 
         // スコアを入れる変数を用意する
-game.score = 0;
-// スコアを表示するラベルを作成
-var scoreLabel = new Label("SCORE : 0");
-scoreLabel.font = "16px Tahoma";
-scoreLabel.color = "white";
-scoreLabel.x = 10;  // X座標
-scoreLabel.y = 5;   // Y座標
-game.rootScene.addChild(scoreLabel);
+        game.score = 0;
+        // スコアを表示するラベルを作成
+        var scoreLabel = new Label("SCORE : 0");
+        scoreLabel.font = "16px Tahoma";
+        scoreLabel.color = "white";
+        scoreLabel.x = 10;  // X座標
+        scoreLabel.y = 5;   // Y座標
+        game.rootScene.addChild(scoreLabel);
 
         var dragon = new Sprite(80, 80);
         dragon.image = game.assets['./enchant.js-builds-0.8.0/images/monster/bigmonster1.gif'];
@@ -45,12 +44,12 @@ game.rootScene.addChild(scoreLabel);
                 return;
             }
             //ドラゴンをアニメーションさせる
-                if(game.frame %10 == 0){
+            if(game.frame %10 == 0){
 
-                    this.frameIndex ++;
-                    this.frameIndex %= frameList.length;
-                    this.frame = frameList[this.frameIndex];
-                }
+                this.frameIndex ++;
+                this.frameIndex %= frameList.length;
+                this.frame = frameList[this.frameIndex];
+            }
             if(game.input.right) {
                 dragon.x += 10;
                 dragon.scaleX = -1;
@@ -76,14 +75,14 @@ game.rootScene.addChild(scoreLabel);
                 console.log("rand: " + rand);
                 dropItem.frame = rand;
 
-                 dropItem.onenterframe = function () {
+                dropItem.onenterframe = function () {
                     if (gameFreezeTime > 0) {
                         return;
                     }
-                     this.y += DropVelosity;
+                    this.y += DropVelosity;
 
-                     if(this.within(dragon, 40)) {
-                            this.remove();
+                    if(this.within(dragon, 40)) {
+                        this.remove();
 
                         if(this.frame != 0) {
                             dragon.frame = 0;
@@ -98,7 +97,7 @@ game.rootScene.addChild(scoreLabel);
                     if(this.y > game.rootScene.height) {
                         this.remove();
                     }
-                 }
+                }
                 game.rootScene.insertBefore(dropItem, dragon);
 
 
